abec959919ed205fe7c94e1ce6b157f2b2df72c0
Sorulai <57193937+Sorulai@users.noreply.github.com>
2022-08-13 20:16:43 +0300

    feat: add game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,40 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.mid = 0;
+
+    }
 
     setRange(min, max) {
+        // let arr = []
+        // for (let i = min; i <= max; i++) {
+        //     arr.push(i)
+        // }
+        // console.log(arr)
+        // return arr
+        this.min = min;
+        this.max = max;
+
 
     }
 
     guess() {
+        let low = this.min
+        let high = this.max
+        let mid = Math.round((low + high) / 2)
+        this.mid = mid
+        return mid
 
     }
 
     lower() {
+        this.max = this.mid
 
     }
 
     greater() {
+        this.min = this.mid
 
     }
 }
